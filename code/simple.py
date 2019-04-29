1c8cc474a6c4fe6f4d924675a9ab248351554813
sam premutico <sam@DN0a233152.SUNet>
2019-04-29 14:01:22 -0700

    added batch norm/mini batches

--- code/simple.py
+++ code/simple.py
@@ -18,11 +18,12 @@ def prep_data(file=DATA, label='SPEED_LIMIT', split=0.9):
     # Encoding class labels
     class_to_predict = label
     class_mapping = {label:idx for idx,label in enumerate(np.unique(df[class_to_predict]))}
-    print(class_mapping)
+    for idx, label in class_mapping.items():
+        print("Class: {} --> {} kmph Speed Limit".format(str(label), str(idx)))
     df[class_to_predict] = df[class_to_predict].map(class_mapping)
 
-    X = df.drop([class_to_predict], axis=1).as_matrix()
-    y = df[class_to_predict].as_matrix()
+    X = df.drop([class_to_predict], axis=1).values
+    y = df[class_to_predict].values
 
     train_len = int(0.8 * len(y))
     test_len = int(len(y) - train_len)
@@ -32,21 +33,22 @@ def prep_data(file=DATA, label='SPEED_LIMIT', split=0.9):
 class DynamicNet(torch.nn.Module):
     def __init__(self, D_in, H, D_out):
         super(DynamicNet, self).__init__()
-        #self.input_linear = torch.nn.Linear(D_in, H)
-        print("Input dimensions: {}\n# Classes: {}".format(D_in, D_out))
+        print("\nInput dimensions --> {}\n# Classes --> {}".format(D_in, D_out))
         self.input_linear = torch.nn.Linear(D_in, D_out)
-        self.tanh = torch.nn.Tanh()
+        torch.nn.init.xavier_uniform_(self.input_linear.weight)
+        self.batch_norm = torch.nn.BatchNorm1d(D_out)
         #self.middle_linear = torch.nn.Linear(H, H)
         #self.output_linear = torch.nn.Linear(H, D_out)
 
     def forward(self, x):
-        h_lin = F.relu(self.input_linear(x))
+        h_lin = self.input_linear(x)
+        h_lin = self.batch_norm(h_lin)
         #for _ in range(random.randint(0, 3)):
         #h_relu = self.middle_linear(h_relu).clamp(min=0)
         y_pred = h_lin
         return y_pred
 
-def run(data, class_to_predict, split_size, learning_rate=0.05, num_epochs=500): 
+def run(data, class_to_predict, split_size, learning_rate=0.5, num_epochs=500, batch_size=50): 
     
     X_train, X_test, y_train, y_test = prep_data(data, class_to_predict, split_size)
 
@@ -67,23 +69,30 @@ def run(data, class_to_predict, split_size, learning_rate=0.05, num_epochs=500):
             y = Variable(torch.Tensor(y_train).cuda())
         else:
             x = Variable(torch.Tensor(X_train).float())
-            y = Variable(torch.Tensor(y_train).long())    
+            y = Variable(torch.Tensor(y_train).long())  
 
-        y_pred = model(x)
-        #for param in model.parameters():
-        #    print(param.data)
+        current_batch = 0
+        for iteration in range(y.shape[0] // batch_size):
+            batch_x = x[current_batch: current_batch + batch_size]
+            batch_y = y[current_batch: current_batch + batch_size]
+            current_batch += batch_size
+            #batch_x, batch_y
+            #print(len(y), len(x),batch_y, current_batch, current_batch+batch_size)
+            y_pred = model(batch_x)
 
-        loss = criterion(y_pred, y)
+            loss = criterion(y_pred, batch_y)
 
-        # Zero gradients, perform a backward pass, and update the weights.
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        if (epoch) % 100 == 0:
-            print(x[0])
-            print(y_pred[0])
-            print('Epoch [%d/%d] Loss: %.4f' %(epoch + 1, num_epochs, loss.data))
-            
+            # Zero gradients, perform a backward pass, and update the weights.
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if (iteration == 0) and ((epoch) % 100 == 0):
+
+                #for name, param in model.named_parameters():
+                #    print(name, param)
+                print("Predictions: ", y_pred[0].data)
+                print('Epoch [%d/%d] Loss: %.4f' %(epoch + 1, num_epochs, loss.data))
+                
     if torch.cuda.is_available():
         x = Variable(torch.Tensor(X_test).cuda())
         y = torch.Tensor(y_test).long()
@@ -110,4 +119,4 @@ def run(data, class_to_predict, split_size, learning_rate=0.05, num_epochs=500):
     print(confusion_matrix(y,predicted))
 
 if __name__ == "__main__":
-    run(data=DATA, class_to_predict='SPEED_LIMIT', split_size=0.1, learning_rate=0.5, num_epochs=500)
+    run(data=DATA, class_to_predict='SPEED_LIMIT', split_size=0.1, learning_rate=0.5, num_epochs=5)
