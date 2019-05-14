03214fbcdbcec97135b537ac2d80670df1fd5e24
sam premutico <sam@DN0a23013e.SUNet>
2019-05-13 21:37:40 -0700

    got CNN running

--- src/simple_cnn.py
+++ src/simple_cnn.py
@@ -3,41 +3,39 @@ from torch import nn
 import torch.nn.functional as F
 
 class SimpleCNN(torch.nn.Module):
-	def __init__(self, input_size, hidden_size, pool_size=2, num_classes=2, num_layers=1):
+	def __init__(self, input_size, hidden_size, pool_size=2, num_classes=2, D=76, num_layers=1):
 		super(SimpleCNN, self).__init__()
 		self.input_size = input_size
 		# A.K.A num output channels = num filters to apply
 		self.hidden_size = hidden_size
 		self.num_layers = num_layers
 		self.num_classes = num_classes
+		self.D = D
 		# TODO: PADDING??
 		self.conv1 = nn.Conv1d(input_size, hidden_size[0], kernel_size=3, padding=1)
 		self.pool_1 = nn.MaxPool1d(kernel_size=2)
-		self.conv2 = nn.Conv1d(hidden_size[0]/float(pool_size), hidden_size[1], kernel_size=3, padding=1)
+		self.conv2 = nn.Conv1d(hidden_size[1], hidden_size[1], kernel_size=3, padding=1)
 		self.pool_2 = nn.MaxPool1d(kernel_size=2)
 
 		self.relu = F.relu
-
-		self.projection = nn.Linear(8000, self.num_classes)
+		out = int(hidden_size[1]/2)
+		print(out)
+		self.projection = nn.Linear(out*self.D, self.num_classes)
 		
 	def forward(self, x):
-		print("Input : {}".format(x.shape))
-		x_conv_1_out = self.conv1(x).permute(0,2,1)
-		print("Conv1 : {}".format(x_conv_1_out.shape))
-		x_pool_1 = self.pool_1(x_conv_1_out).permute(0,2,1)
-		print("Pool1 : {}".format(x_pool_1.shape))
-		x_layer_1_out = self.relu(x_pool_1)
+		#print("Input : {}".format(x.shape))
+		x_conv_1_out = self.conv1(x)
+		x_layer_1_out = self.relu(x_conv_1_out).permute(0,2,1)
+		x_pool_1 = self.pool_1(x_layer_1_out).permute(0,2,1)
 
-		x_conv_2_out = self.conv2(x_layer_1_out).permute(0,2,1)
-		print("Conv2 : {}".format(x_conv_2_out.shape))
-		x_pool_2 = self.pool_2(x_conv_2_out).permute(0,2,1)
-		print("Pool2 : {}".format(x_pool_2.shape))
-		x_layer_2_out = self.relu(x_pool_2)
+		x_conv_2_out = self.conv2(x_pool_1)
+		x_layer_2_out = self.relu(x_conv_2_out).permute(0,2,1)
+		x_pool_2 = self.pool_2(x_layer_2_out).permute(0,2,1)
 
-		N, F, C = x_layer_2_out.shape
-		x_final = x_layer_2_out.reshape(1,N*F*C)
+		N, T, D = x_pool_2.shape
+		x_final = x_pool_2.reshape(N,T*D)
 
-		print("Reshaped : {}".format(x_final.shape))
+		#print("Reshaped : {}".format(x_final.shape))
 
 		scores = self.projection(x_final)
 		
--- src/train_simple_cnn.py
+++ src/train_simple_cnn.py
@@ -10,10 +10,14 @@ from utils import load_pytorch_data, metrics, check_validation_accuracy
 #hidden_size = 50
 
 INPUT_SIZE = 150#103
+D = 76
 hidden_size = [80,40]
+
+losses = list()
+
 def train():
   train_data, validation_data = load_pytorch_data(batch_size=32)
-  model = SimpleCNN(INPUT_SIZE, hidden_size)
+  model = SimpleCNN(input_size=INPUT_SIZE, D=D, hidden_size=hidden_size)
   optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
   loss_weights = torch.tensor([0.1, 0.9])
   criterion = torch.nn.CrossEntropyLoss(weight=loss_weights)
@@ -34,6 +38,7 @@ def train():
       if iter % 40 == 0:
         print('Iter {} loss: {}'.format(iter, loss.item()))
         check_validation_accuracy(model, validation_data)
+        losses.append(loss.item())
 
   x = [i for i in range(len(losses))]
   plt.plot(x, losses)
